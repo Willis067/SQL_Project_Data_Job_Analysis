fe217780cde1510227dd536f18b47958d1bf8ac2
Willis <a0729ts@gmail.com>
2026-02-14 02:57:42 +0800

    delected: project_sql/query_1.sql

---
+++ project_sql/2_top_paying_job_skills.sql
@@ -0,0 +1,31 @@
+WITH top_paying_jobs AS(
+    SELECT
+        job_id,
+        job_title,
+        job_location,
+        job_schedule_type,
+        salary_year_avg,
+        job_posted_date,
+        name AS company_name
+    FROM
+        job_postings_fact
+    LEFT JOIN company_dim
+        USING (company_id)
+    WHERE
+        job_title_short = 'Data Analyst' 
+        AND job_location = 'Hong Kong' 
+        AND salary_year_avg IS NOT NULL
+    ORDER BY 
+        salary_year_avg DESC
+    LIMIT 10
+)
+SELECT
+    top_paying_jobs.*,
+    skills
+FROM skills_job_dim 
+JOIN top_paying_jobs
+    USING (job_id)
+JOIN skills_dim
+    USING (skill_id)
+ORDER BY
+    salary_year_avg DESC
